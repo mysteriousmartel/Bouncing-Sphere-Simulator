8d6e8c079cb53402acd45c86c460b8e5af2a9796
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-11-04 16:56:38 -0500

    Add files via upload

--- spheres.py
+++ spheres.py
@@ -24,11 +24,23 @@ print(mass, " ", radius, " ", x_0, " ", y_0, " ", z_0, " ", vx_0, " ", vy_0, " "
 each value to the corresponding component (mass, radius, etc.), where all of these
 components are strings or arrays'''
 
-print('Enter for each sphere as: mass, radius, x_0, y_0, z_0, vx_0, vy_0, vz_0')
+print('Enter for each sphere as: mass, radius, x_0, y_0, z_0, vx_0, vy_0, vz_0, sphere number')
 
-mass, radius, x_0, y_0, z_0, vx_0, vy_0, vz_0 = # initialize all as strings
+name = []
 
 for line in sys.stdin:
+
   init_val = line.split(" ")
 
-  mass = mass.append(init_val[0])
+  name = name.append(init_val[8])
+
+  initials[line] = Ball(init_val[0],init_val[1],[init_val[2],init_val[3],init_val[4]],[init_val[5],init_val[6],init_val[7]])
+
+'''mass = mass.append(init_val[0])
+  radius = radius.append(init_val[1])
+  x_0 = x_0.append(init_val[2])
+  y_0 = y_0.append(init_val[3])
+  z_0 = z_0.append(init_val[4])
+  vx_0 = vx_0.append(init_val[5])
+  vy_0 = vy_0.append(init_val[6])
+  vz_0 = vz_0.append(init_val[7])'''
