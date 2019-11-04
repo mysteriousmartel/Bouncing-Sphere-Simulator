1df962dbdd4ce8a27f80ecafd635c7e36e49a194
HaikM17 <55334624+HaikM17@users.noreply.github.com>
2019-11-04 12:52:03 -0500

    Create hm_spheres.py

---
+++ hm_spheres.py
@@ -0,0 +1,28 @@
+import numpy as np
+import sys, os
+from decimal import Decimal
+import copy
+import math
+from timeit import default_timer as timer
+
+
+#creates class for making the balls
+
+
+class Ball():
+  def __init__(self, mass, radius, name, pos=[], vel=[]):
+    self.mass = mass
+    self.radius = radius
+    self.name = name
+    self.pos = np.array(pos, np.float64)
+    self.vel = np.array(vel, np.float64)
+    self.time = 0.00000000
+
+  def __repr__(self):
+    return str(self.name)+" "+str(self.pos)[1:-1]+" "+str(self.vel)[1:-1]
+
+  def updatePos(self, t): 
+    # dt = (t - self.time)
+    dt = float(Decimal(t) - Decimal(self.time))
+    self.pos += dt * self.vel
+    self.time = t
