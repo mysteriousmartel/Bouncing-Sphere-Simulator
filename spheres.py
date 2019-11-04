a3d3d3d4be2b5935bb16bcd7c0ec15894394f500
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-11-04 16:57:50 -0500

    Add files via upload

--- spheres.py
+++ spheres.py
@@ -1,25 +1,6 @@
 import sys
 import numpy
 
-'''For the case of one sphere
-
-print('Please enter initial values on individual lines for each sphere:')
-
-args = sys.stdin.read()
-
-init_val = args.split()
-
-mass = int(init_val[0])
-radius = int(init_val[1])
-x_0 = int(init_val[2])
-y_0 = int(init_val[3])
-z_0 = int(init_val[4])
-vx_0 = int(init_val[5])
-vy_0 = int(init_val[6])
-vz_0 = int(init_val[7])
-
-print(mass, " ", radius, " ", x_0, " ", y_0, " ", z_0, " ", vx_0, " ", vy_0, " ", vz_0)'''
-
 '''To take in all values, read lines individually and split, then use a loop to assign
 each value to the corresponding component (mass, radius, etc.), where all of these
 components are strings or arrays'''
