7a5cff673e20df36145ab7d37e3ffef9ae8c3dd4
HaikM17 <55334624+HaikM17@users.noreply.github.com>
2019-11-05 12:59:26 -0500

    Update hm_spheres.py

--- hm_spheres.py
+++ hm_spheres.py
@@ -6,9 +6,6 @@ import math
 from timeit import default_timer as timer
 
 
-#creates class for making the balls
-
-
 class Ball():
   def __init__(self, mass, radius, name, pos=[], vel=[]):
     self.mass = mass
@@ -21,22 +18,63 @@ class Ball():
   def __repr__(self):
     return str(self.name)+" "+str(self.pos)[1:-1]+" "+str(self.vel)[1:-1]
 
-  def updatePos(self, t): 
+  def updatepos(self, t): 
     # dt = (t - self.time)
     dt = float(Decimal(t) - Decimal(self.time))
     self.pos += dt * self.vel
     self.time = t
-
-    
-    # Universe Class
-    
-      #constructor
-      #collision checker
-      #collision time
-      #collisions 
-      #update ball position
-      #update ball velocity
-      #update time
-      
     
     
+class Universe():
+    def __init__(self, radius, max_col, ball_array):
+    #constructor
+        self.radius=radius
+        self.max_col=max_col
+        self.ball_array=copy.deepcopy(ball_array)
+        self.time = 0.00000000
+        
+    def iscollision(auto=True):
+    #collision checker
+    
+        if universe:
+            per_refl
+        elif ball:
+            elastic
+            
+        
+    def collision_time():
+    #collision time
+    
+    def collisions():
+    #collisions    
+        
+    
+    def update_pos(self, t):
+    #update all ball positions
+    for ball in self.ball_array:
+		ball.updatepos(t)
+    
+        
+    def update_vel(self, ball1, ball2):
+    #update ball velocity
+    p1, p2= ball1.pos, ball2.pos
+    v1, v2= ball1.vel, ball2.vel
+    m1, m2 = ball1.mass, ball2.mass
+    
+    vp=  np.subtract(v1,v2)
+    v_p= np.subtract(v2-v1)
+    
+    rp=  np.subtract(p1-p2)
+    r_p= np.subtract(p2-p1)
+    
+    mp=  np.subtract(m1-m2)
+    m_p= p.subtract(m2-m1)
+    
+    ball1.vel= v1 - (2.*m2/(m1+m2)) * (np.dot(vp,rp)/np.dot(rp,rp))*rp 
+    ball2.vel= v2 - (2.*m1/(m1+m2)) * (np.dot(v_p,r_p)/np.dot(r_p,r_p))*r_p 
+    
+    
+    
+    
+    def update_time():
+    #update time
