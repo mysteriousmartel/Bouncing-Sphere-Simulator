41599f07f5873bc429722d9cc1e3e4783cbb1f3c
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-11-05 12:57:10 -0500

    Add files via upload

--- spheres.py
+++ spheres.py
@@ -1,27 +1,59 @@
-import sys
-import numpy
+import numpy as np
+import sys, os
+from decimal import Decimal
+import copy
+import math
+from timeit import default_timer as timer
+
+
+#creates class for making the balls
+
+
+class Ball():
+  def __init__(self, mass, radius, name, pos=[], vel=[]):
+    self.mass = mass
+    self.radius = radius
+    self.name = name
+    self.pos = np.array(pos, np.float64)
+    self.vel = np.array(vel, np.float64)
+    self.time = 0.00000000
+
+  def __repr__(self):
+    return str(self.name)+" "+str(self.pos)[1:-1]+" "+str(self.vel)[1:-1]
+
+  def updatePos(self, t): 
+    # dt = (t - self.time)
+    dt = float(Decimal(t) - Decimal(self.time))
+    self.pos += dt * self.vel
+    self.time = t
 
 '''To take in all values, read lines individually and split, then use a loop to assign
 each value to the corresponding component (mass, radius, etc.), where all of these
 components are strings or arrays'''
 
-print('Enter for each sphere as: mass, radius, x_0, y_0, z_0, vx_0, vy_0, vz_0, sphere number')
+if __name__ == "__main__":
 
-name = []
+  univ_rad = sys.argv[0]
+  univ_coll = sys.argv[1]
 
-for line in sys.stdin:
+  universe = Universe(univ_rad,univ_coll)
 
-  init_val = line.split(" ")
+  print("Please enter the mass, radius, x/y/z position, x/y/z velocity", "\n", "and name of each sphere", "\n", "When complete, use EOF / Ctrl-D to stop entering")
 
-  name = name.append(init_val[8])
+  name = []
+  initials = []
+  names = []
 
-  initials[line] = Ball(init_val[0],init_val[1],[init_val[2],init_val[3],init_val[4]],[init_val[5],init_val[6],init_val[7]])
+  for line in sys.stdin:
 
-'''mass = mass.append(init_val[0])
-  radius = radius.append(init_val[1])
-  x_0 = x_0.append(init_val[2])
-  y_0 = y_0.append(init_val[3])
-  z_0 = z_0.append(init_val[4])
-  vx_0 = vx_0.append(init_val[5])
-  vy_0 = vy_0.append(init_val[6])
-  vz_0 = vz_0.append(init_val[7])'''
+    init_val = line.split(" ")
+
+    name.append(init_val[8])
+
+    initials.append(Ball(init_val[0],init_val[1],init_val[8],[init_val[2],init_val[3],init_val[4]],[init_val[5],init_val[6],init_val[7]]))
+
+  # for i in range(len(initials)):
+  #   names = initials[i].name.replace("\n", '')
+  #   print(names)
+
+  
